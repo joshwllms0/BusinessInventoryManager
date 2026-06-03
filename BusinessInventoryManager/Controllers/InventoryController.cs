fede4efa7e1bf57cafed23315141776c03042ed4
RealHasty <joshua.williams@southeasttech.edu>
2026-06-02 19:14:34 -0500

    Add project files.

---
+++ BusinessInventoryManager/Controllers/InventoryController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BusinessInventoryManager.Models;
+
+namespace BusinessInventoryManager.Controllers
+{
+    public class InventoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BusinessInventoryManager/Models/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessInventoryManager.Models
+{
+    public class Inventory
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string ItemName { get; set; } = string.Empty;
+        [StringLength(1000)]
+        public string? ItemDescription { get; set; } = null;
+        public Category Category { get; set; }
+        [Required]
+        [Range(0, 99999)]
+        public int Stock { get; set; }
+        [Required]
+        [Range(0.01, 9999)]
+        public decimal Price { get; set; }
+    }
+
+    public enum Category
+    {
+        Electronics,
+        Clothing,
+        Books,
+        Food
+        // Etc.
+    }
+}
